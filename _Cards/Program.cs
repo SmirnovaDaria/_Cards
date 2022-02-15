18853bdfe2cd57370990aa5015c278647a905b5e
Даша <@DESKTOP-S73BU2I>
2022-02-15 16:12:41 +0300

    Добавьте файлы проекта.

---
+++ _Cards/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _Cards
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var startingDeck = (from s in Suits().LogQuery("Suit Generation")
+                                from r in Ranks().LogQuery("Value Generation")
+                                select new { Suit = s, Rank = r })
+                       .LogQuery("Starting Deck")
+                       .ToArray();
+
+            foreach (var c in startingDeck)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine();
+
+            var times = 0;
+            var shuffle = startingDeck;
+
+            do
+            {
+                /*
+                shuffle = shuffle.Take(26)
+                    .LogQuery("Top Half")
+                    .InterleaveSequenceWith(shuffle.Skip(26).LogQuery("Bottom Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
+                */
+
+                shuffle = shuffle.Skip(26)
+                    .LogQuery("Bottom Half")
+                    .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
+
+                foreach (var c in shuffle)
+                {
+                    Console.WriteLine(c);
+                }
+
+                times++;
+                Console.WriteLine(times);
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+            Console.ReadKey();
+        }
+        static IEnumerable<string> Suits()
+        {
+            yield return "Буби";
+            yield return "Вини";
+            yield return "Черви";
+            yield return "Крести";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "2";
+            yield return "3";
+            yield return "4";
+            yield return "5";
+            yield return "6";
+            yield return "7";
+            yield return "8";
+            yield return "9";
+            yield return "10";
+            yield return "Валет";
+            yield return "Дама";
+            yield return "Король";
+            yield return "Туз";
+        }
+    }
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>
+        (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+        public static bool SequenceEquals<T>
+        (this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static IEnumerable<T> LogQuery<T>
+        (this IEnumerable<T> sequence, string tag)
+        {
+            // File.AppendText creates a new file if the file doesn't exist.
+            using (var writer = File.AppendText("debug.log"))
+            {
+                writer.WriteLine($"Executing Query {tag}");
+            }
+
+            return sequence;
+        }
+    }
+}
